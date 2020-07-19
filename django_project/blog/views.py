cf4e2a608b966451e707bd542e47fdbe87f8963a
DevJ5 <idevelopjs@gmail.com>
2020-07-19 11:47:34 +0200

    Templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'DevJ5',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'Juli 18, 2020'
+    },
+    {
+        'author': 'Mario',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'Juli 19, 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
--- environment.md
+++ environment.md
@@ -28,3 +28,12 @@ python manage.py startapp blog
 - create urls.py
 - create a route in views.py
 - add an item to project urls.py to forward to app urls
+- create templates folder + create another folder for our html files
+- in settings.py add our blog app class to INSTALLED_APPS list as blog.apps.BlogConfig
+- in views.py we return render(request, 'blog/home.html', context)
+- the last argument of render is an object we can pass down to the template, in this case context
+- use {% for post in posts %} to start for loop and {% endfor %} to end (jinja style)
+- variables are accessed with doubly curly {{post.title}}
+- use template blocks to reuse other templates (template inheritance)
+
+
